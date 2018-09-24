0a38f8c1daf348fdc8259a3434746a9ad2b24ab9
g3ry89 <gery89@gmail.com>
2018-09-24 13:41:41 +0200

    Merge branch 'master' of https://github.com/dewsy/poker-player-team-korda
    # Conflicts:
    #	player.py

--- player.py
+++ player.py
@@ -2,16 +2,13 @@ class Player:
     VERSION = "1.0 Black Widow"
 
     def betRequest(self, game_state):
-        my_cards = []
-        for i in game_state["players"]:
-            if i["name"] == "team Korda":
-                my_cards.append(i["hole_cards"])
-        my_cards.append(game_state["community_cards"])
+        us = game_state["players"][game_state["in_action"]]
+        hold = game_state["current_buy_in"] - us["bet"]
+        my_cards = us["hole_cards"] + game_state["community_cards"]
         for card in my_cards:
-            for card2 in my_cards:
-                if card["rank"] == card2["rank"]:
-                    return 1000
-        return 10
+            if card["rank"] in "2 3 4 5 6 7 8 9":
+                return hold
+        return hold + game_state["minimum_raise"] + 10
 
     def showdown(self, game_state):
         pass
