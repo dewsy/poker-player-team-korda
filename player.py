f1cfbce97f7d48171e00b8013273c54c287db42f
g3ry89 <gery89@gmail.com>
2018-09-24 14:09:46 +0200

    drill

--- player.py
+++ player.py
@@ -5,10 +5,22 @@ class Player:
         us = game_state["players"][game_state["in_action"]]
         hold = game_state["current_buy_in"] - us["bet"]
         my_cards = us["hole_cards"] + game_state["community_cards"]
+
         for card in my_cards:
-            if card["rank"] in "2 3 4 5 6 7 8 9":
+            if card["rank"] in "J Q K A":
                 return hold
         return hold + game_state["minimum_raise"] + 10
 
     def showdown(self, game_state):
         pass
+
+    def check_drill(self, my_cards):
+        seen = set()
+        uniq = []
+        for x in my_cards:
+            if x not in seen:
+                uniq.append(x)
+                seen.add(x)
+                if uniq - seen == 3:
+                    return True
+        return False
