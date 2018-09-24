559e39cb89daa8f6b22076452b9fa81a4c10fd0e
g3ry89 <gery89@gmail.com>
2018-09-24 11:59:06 +0200

    Mergeg

--- player.py
+++ player.py
@@ -1,10 +1,18 @@
 
-
 class Player:
-    VERSION = "0.9 Hawkeye"
+    VERSION = "1.0 Black Widow"
 
     def betRequest(self, game_state):
-        return 1000
+        my_cards = []
+        for i in game_state["players"]:
+            if i["name"] == "team Korda":
+                my_cards.append(i["hole_cards"])
+        my_cards.append(game_state["community_cards"])
+        for card in my_cards:
+            for card2 in my_cards:
+                if card["rank"] == card2["rank"]:
+                    return 1000
+        return 10
 
     def showdown(self, game_state):
         pass
