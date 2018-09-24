85f47e5b9e175d0e5c11d4f52d57a9bef118151f
dewsy <dewsyy@gmail.com>
2018-09-24 12:08:34 +0200

    revert

--- player.py
+++ player.py
@@ -2,16 +2,9 @@ class Player:
     VERSION = "1.0 Black Widow"
 
     def betRequest(self, game_state):
-        my_cards = []
-        for i in game_state["players"]:
-            if i["name"] == "team Korda":
-                my_cards.append(i["hole_cards"])
-        my_cards.append(game_state["community_cards"])
-        for card in my_cards:
-            for card2 in my_cards:
-                if card["rank"] == card2["rank"]:
-                    return 1000
-        return 10
+        return 1000
+
+
 
     def showdown(self, game_state):
         pass
